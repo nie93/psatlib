89b7a51734a84966d88e884bac8bc5c58363f903
nie93 <nie@vt.edu>
2018-06-13 15:50:03 -0700

    Added reset_loadid() and apply_changes() functions

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # psatlib
 
 ## Descriptions
-psatlib is an imported library designed for PSAT running with Python scripts
+`psatlib` is an imported library designed for PSAT running with Python scripts.
 
 
-## How To Use It
+## How To Use It (Command Line Interface)
 1. Copy `psatlib.py` to your PSAT's python userscripts directory (e.g. `C:\DSATools_18-SL\Psat\bin\python\UserScripts`).
 
-2. Open command window (`cmd.exe`), and type in
+2. Open command window (`cmd.exe`), change directory to the Python script folder you used, then type in
 
 ```
 PSAT "test.py" python
@@ -16,8 +16,21 @@ PSAT "test.py" python
 
 ## Development Guide
 * [Google Python Style Guide](https://google.github.io/styleguide/pyguide.html)
+* [PEP-8](https://www.python.org/dev/peps/pep-0008/)
 * Importing external packages (e.g. NumPy) is **NOT** recommended except for better computing performance. You may import external packages in your own scripts.
 
 
 ## List of Functions
 * get_busnum(ctype)
+
+## Known Bugs
+
+### Problem with Python 2.7.x Environment
+PSAT GUI returns RunTime Error with PSAT under 
+
+* Python 2.7.15 (v2.7.15:ca079a3ea3, Apr 30 2018, 16:30:26) [MSC v.1500 64 bit (AMD64)] on win32 
+
+No problem with 
+
+* Python 2.7.9 (default, Dec 10 2014, 12:24:55) [MSC v.1500 32 bit (Intel)] on win32 
+* Python 2.7.13 (v2.7.13:a0645b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32 
--- psatlib.py
+++ psatlib.py
@@ -15,150 +15,225 @@ __description__ = "psatlib is an imported library designed for PSAT running with
 from psat_python27 import *
 error = psat_error()
 
-# Returns the list of bus numbers
+# Resets all load id as the same string "1"
+def reset_loadid():
+    f = psat_comp_id(ctype.ld,1,'')
+    more = get_next_comp('mainsub',f,error)
+    while more == True:
+        c = get_load_dat(f,error)
+        c.id = "1"
+        set_load_dat(f,c,error)
+        more = get_next_comp('mainsub',f,error)    
+
+
+# Returns a list of bus numbers of certain type of components
 def get_busnum(ct):
-	f = psat_comp_id(ct,1,'')
-	bn = []
-	morebus = get_next_comp('mainsub',f,error)
-	while morebus == True:
-		bn.append(f.bus)
-		morebus = get_next_comp('mainsub',f,error)
-	bn.sort()
-	return bn
+    f = psat_comp_id(ct,1,'')
+    bn = []
+    more = get_next_comp('mainsub',f,error)
+    while more == True:
+        bn.append(f.bus)
+        more = get_next_comp('mainsub',f,error)
+    bn.sort()
+    return bn
 
 # Returns loads (designed for single load component)
 def get_loads(busnum):
-	mw = []
-	mvar = []
-	for i in range(len(busnum)):
-		c = get_load_dat(busnum[i],"1",error)
-		mw.append(c.mw)	
-		mvar.append(c.mvar)
-	return {'p':mw, 'q':mvar}
+    if type(busnum) == int:
+        c = get_load_dat(busnum,"1",error)
+        mw = c.cp[0]
+        mvar = c.cq[0]
+    else:
+        mw = []
+        mvar = []
+        for i in range(len(busnum)):
+            c = get_load_dat(busnum[i],"1",error)
+            mw.append(c.cp[0])
+            mvar.append(c.cq[0])
+    return {'p':mw, 'q':mvar}
 
 # Returns aggregated loads by area (designed for single load component)
 def get_aggloads_by_area(areanum):
-	areanum = list(set(areanum))
-	areanum.sort()
-	mw = [0.] * len(areanum)
-	mvar = [0.] * len(areanum)
-	f = psat_comp_id(ctype.ld,1,'')
-	moreload = get_next_comp('mainsub',f,error)
-	while moreload == True:
-		loaddat = get_load_dat(f,error)
-		mw[areanum.index(loaddat.area)] += loaddat.mw
-		mvar[areanum.index(loaddat.area)] += loaddat.mvar
-		moreload = get_next_comp('mainsub',f,error)
-	return {'area': areanum, 'p':mw, 'q':mvar}
+    areanum = list(set(areanum))
+    areanum.sort()
+    mw = [0.] * len(areanum)
+    mvar = [0.] * len(areanum)
+    f = psat_comp_id(ctype.ld,1,'')
+    more = get_next_comp('mainsub',f,error)
+    while more == True:
+        c = get_load_dat(f,error)
+        if c.status == 1:
+            mw[areanum.index(c.area)] += c.mw
+            mvar[areanum.index(c.area)] += c.mvar
+        more = get_next_comp('mainsub',f,error)
+    return {'area': areanum, 'p':mw, 'q':mvar}
 
 # Set area numbers for all buses
 def set_bus_areas(areanum):
-	nb = get_count_comp(ctype.bs,error)
-	if nb != len(areanum):
-		return
-	else:
-		nar_before = get_count_comp(ctype.ar,error)
-		nar_after = len(set(areanum))
-		busnum = range(1,1+nb)
-		for i in range(nar_after - nar_before):
-			newarea = psat_area_dat()
-			newarea.number = nar_before + i + 1
-			newarea.name = str(nar_before + i + 1)
-			add_comp(newarea,error)
-		for i in range(len(areanum)):
-			busdat = get_bus_dat(busnum[i],error)
-			loaddat = get_load_dat(busnum[i],"1",error)
-			busdat.area = areanum[i]
-			loaddat.area = areanum[i]
-			set_bus_dat(busnum[i],busdat,error)
-			set_load_dat(busnum[i],"1",loaddat,error)
+    nb = get_count_comp(ctype.bs,error)
+    if nb != len(areanum):
+        return
+    else:
+        nar_before = get_count_comp(ctype.ar,error)
+        nar_after = len(set(areanum))
+        busnum = range(1,1+nb)
+        for i in range(nar_after - nar_before):
+            newarea = psat_area_dat()
+            newarea.number = nar_before + i + 1
+            newarea.name = str(nar_before + i + 1)
+            add_comp(newarea,error)
+        for i in range(len(areanum)):
+            busdat = get_bus_dat(busnum[i],error)
+            loaddat = get_load_dat(busnum[i],"1",error)
+            busdat.area = areanum[i]
+            loaddat.area = areanum[i]
+            set_bus_dat(busnum[i],busdat,error)
+            set_load_dat(busnum[i],"1",loaddat,error)
 
 # Returns the list of area numbers
 def get_areanum(busnum):
-	area = []
-	for i in range(len(busnum)):
-		c = get_bus_dat(busnum[i],error)
-		area.append(c.area)	
-	return area
+    area = []
+    for i in range(len(busnum)):
+        c = get_bus_dat(busnum[i],error)
+        area.append(c.area)    
+    return area
 
 # Displays components information
-def display_comp_msg(ct):
-	f = psat_comp_id(ct,1,'')
-	morecomp = get_next_comp('mainsub',f,error)
-	while morecomp == True:
-		if f.type == ctype.ld:
-			c = get_load_dat(f,error)
-			msg_str = '%9s AT BUS #%3d (AREA #%2d), {P:%8.2f, Q:%8.2f}' \
-					%('LOAD', c.bus, c.area, c.mw, c.mvar)
-		elif f.type == ctype.gen:
-			c = get_gen_dat(f,error)
-			msg_str = '%9s AT BUS #%3d, {P:%8.2f, Q:%8.2f} ST:%d' \
-					%('GENERATOR', c.bus, c.mw, c.mvar, c.status)		
-		psat_msg(msg_str)
-		morecomp = get_next_comp('mainsub',f,error)
+def disp_comp_msg(ct):
+    f = psat_comp_id(ct,1,'')
+    more = get_next_comp('mainsub',f,error)
+    while more == True:
+        if f.type == ctype.ld:
+            c = get_load_dat(f,error)
+            msg_str = '%9s AT BUS #%3d (AREA #%2d), {P:%8.2f, Q:%8.2f}' \
+                    %('LOAD', c.bus, c.area, c.mw, c.mvar)
+        elif f.type == ctype.gen:
+            c = get_gen_dat(f,error)
+            msg_str = '%9s AT BUS #%3d, {P:%8.2f, Q:%8.2f} ST:%d' \
+                    %('GENERATOR', c.bus, c.mw, c.mvar, c.status)        
+        psat_msg(msg_str)
+        more = get_next_comp('mainsub',f,error)
 
 # Scales load
-def scale_loads(busnum, x):
-	for i in range(len(busnum)):
-		c = get_load_dat(busnum[i],"1",error)
-		c.cp[0] = x * c.cp[0]
-		c.cq[0] = x * c.cq[0]
-		set_load_dat(busnum[i],"1",c,error)
+def scale_loads(subsys, x):
+    f = psat_comp_id(ctype.ld,1,'')
+    more = get_next_comp(subsys,f,error)
+    while more == True:
+        c = get_load_dat(f,error)
+        # c = get_load_dat(busnum[i],"1",error)
+        c.cp[0] = x * c.cp[0]
+        c.cq[0] = x * c.cq[0]
+        set_load_dat(f,c,error)
+        more = get_next_comp(subsys,f,error)
 
 # Returns the summation of loads 
 def get_sum_load(subsys):
-	mw = 0.
-	mvar = 0.
-	f = psat_comp_id(ctype.ld,1,'')
-	more = get_next_comp(subsys,f,error)
-	while more == True:
-		loaddat = get_load_dat(f,error)
-		mw += loaddat.mw
-		mvar += loaddat.mvar
-		more = get_next_comp(subsys,f,error)
-	return {'p':mw, 'q':mvar}
+    mw = 0.
+    mvar = 0.
+    f = psat_comp_id(ctype.ld,1,'')
+    more = get_next_comp(subsys,f,error)
+    while more == True:
+        loaddat = get_load_dat(f,error)
+        mw += loaddat.mw
+        mvar += loaddat.mvar
+        more = get_next_comp(subsys,f,error)
+    return {'p':mw, 'q':mvar}
 
 # Returns the summation of generators outputs
 def get_sum_genoutput(subsys):
-	mw = 0.
-	mvar = 0.
-	f = psat_comp_id(ctype.gen,1,'')
-	more = get_next_comp(subsys,f,error)
-	while more == True:
-		gendat = get_gen_dat(f,error)
-		mw += gendat.mw
-		mvar += gendat.mvar
-		more = get_next_comp(subsys,f,error)
-	return {'p':mw, 'q':mvar}
+    mw = 0.
+    mvar = 0.
+    f = psat_comp_id(ctype.gen,1,'')
+    more = get_next_comp(subsys,f,error)
+    while more == True:
+        gendat = get_gen_dat(f,error)
+        mw += gendat.mw
+        mvar += gendat.mvar
+        more = get_next_comp(subsys,f,error)
+    return {'p':mw, 'q':mvar}
 
 # Returns the summation of fixed shunt reactive power injection
 def get_sum_fxshmvar(subsys):
-	mvar = 0.
-	f = psat_comp_id(ctype.fxsh,1,'')
-	more = get_next_comp(subsys,f,error)
-	while more == True:
-		fxshdat = get_fx_shunt_dat(f,error)
-		mvar += fxshdat.mvar
-		more = get_next_comp(subsys,f,error)
-	return mvar
+    mvar = 0.
+    f = psat_comp_id(ctype.fxsh,1,'')
+    more = get_next_comp(subsys,f,error)
+    while more == True:
+        fxshdat = get_fx_shunt_dat(f,error)
+        mvar += fxshdat.mvar
+        more = get_next_comp(subsys,f,error)
+    return mvar
 
 # Rescales generator outputs (fixed shunt reactive power injection considered)
 def rescale_gens(subsys):
-	genoutput = get_sum_genoutput(subsys)
-	load = get_sum_load(subsys)
-	fxshmvar = get_sum_fxshmvar(subsys)
-	psat_msg(str(fxshmvar))
-	xp = load['p'] / genoutput['p'] 
-	xq = load['q'] / (genoutput['q'] + fxshmvar)
-	f = psat_comp_id(ctype.gen,1,'')
-	more = get_next_comp(subsys,f,error)
-	while more == True:
-		c = get_gen_dat(f,error)
-		c.mw = xp * c.mw
-		c.mvar = xq * c.mvar
-		set_gen_dat(f,c,error)
-		more = get_next_comp(subsys,f,error)
-	
+    genoutput = get_sum_genoutput(subsys)
+    load = get_sum_load(subsys)
+    fxshmvar = get_sum_fxshmvar(subsys)
+    xp = load['p'] / genoutput['p'] 
+    xq = load['q'] / (genoutput['q'] + fxshmvar)
+    f = psat_comp_id(ctype.gen,1,'')
+    more = get_next_comp(subsys,f,error)
+    while more == True:
+        c = get_gen_dat(f,error)
+        c.mw = xp * c.mw
+        c.mvar = xq * c.mvar
+        set_gen_dat(f,c,error)
+        more = get_next_comp(subsys,f,error)
+    
 # Generates TSAT files for transient stability analysis (psb,dyr,swi,mon)
 def generate_tsa(path,psb,dyr,swi,mon):
-	return
+    return
+
+# Displays a list in PSAT message tab
+def disp_list(l):
+    for i in range(len(l)):
+        psat_msg(str(l[i]))
+
+# Returns a list of bus numbers in a specified area
+def get_busnum_in_area(areanum):
+    f = psat_comp_id(ctype.bs,1,'')
+    bn = []
+    more = get_next_comp('mainsub',f,error)
+    while more == True:
+        c = get_bus_dat(f,error)
+        if c.area == areanum:
+            bn.append(c.number)
+        more = get_next_comp('mainsub',f,error)
+    bn.sort()
+    return bn
+
+# Returns the load in a specified area
+def get_load_in_area(areanum):
+    f = psat_comp_id(ctype.ld,1,'')
+    mw = 0.
+    mvar = 0.
+    more = get_next_comp('mainsub',f,error)
+    while more == True:
+        c = get_load_dat(f,error)
+        if (c.area == areanum and c.status == 1):
+            mw += c.cp[0]
+            mvar += c.cq[0]
+        more = get_next_comp('mainsub',f,error)
+    return {'p': mw, 'q':mvar}
+    
+
+# Applys a set of changes to the imported case (inspired by MATPOWER)
+def apply_changes(lbl, chgtbl):
+    chgid = [i for i, x in enumerate(chgtbl) if x[0] == lbl]
+    for i in chgid:
+        areaload = get_load_in_area(chgtbl[i][2])
+        # psat_msg('LABEL#%3d  %s  %2d %s  %s [%8.2f -> %8.2f]' \
+        #          %(chgtbl[i][0], chgtbl[i][1], chgtbl[i][2], chgtbl[i][3], \
+        #            chgtbl[i][4], areaload['p'], chgtbl[i][5]))
+        if chgtbl[i][1] == 'AREALOAD':
+            bn = get_busnum_in_area(chgtbl[i][2])
+            for bi in range(len(bn)):
+                c = get_load_dat(bn[bi], "1", error)
+                if chgtbl[i][3] == 'P':
+                    c.cp[0] = c.cp[0] * chgtbl[i][5] / areaload['p']
+                elif chgtbl[i][3] == 'PQ':
+                    c.cp[0] = c.cp[0] * chgtbl[i][5] / areaload['p']
+                    c.cq[0] = c.cq[0] * chgtbl[i][5] / areaload['q']
+                set_load_dat(bn[bi], "1", c, error)
+        elif chgtbl[i][1] == 'BUSLOAD':
+            return
+    return
